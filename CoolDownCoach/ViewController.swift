a90e16e0a0e7fd654e9798df8f4379d548ef6cf4
NewSouthWave <tigers0110@naver.com>
2025-02-08 10:26:24 +0900

    Initial Commit

---
+++ CoolDownCoach/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoolDownCoach
+//
+//  Created by Nam on 2/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
